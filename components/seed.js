832de7045f3883c65f1bb412e5b476355a0838f7
endows <you@example.com>
2015-07-24 12:38:56 +0900

    oil-components

--- components/seed.js
+++ components/seed.js
@@ -1,24 +1,36 @@
 var Seed = BlazeComponent.extendComponent({
-  template: function () {
+  template: function() {
     return 'Seed';
   },
 
-  fires:new ReactiveArray([1,2,3]),
+  fires: new ReactiveArray([1, 2, 3]),
 
-  explosion:function(e){
+  explosion: function(e) {
+    if(Session.get('oil_count') == 0){
+      return false
+    }
     this.fires.unshift('0')
     $('.burn').last().velocity({
       "color": "#FE642E"
     }).velocity("reverse", {
       "delay": 500
     });
+    Session.set('oil_count',Session.get('oil_count') - 1)
   },
 
-  fire_count:function(){
+  fire_count: function() {
     return this.fires.list()
   },
 
-  events: function () {
+  oil_count: function() {
+    var arr = []
+    for (var i = 0; i < Session.get('oil_count'); i++) {
+      arr[i] = i;
+    }
+    return arr
+  },
+
+  events: function() {
     return Seed.__super__.events.call(this).concat({
       'click a.tooltipped': this.explosion
     });
